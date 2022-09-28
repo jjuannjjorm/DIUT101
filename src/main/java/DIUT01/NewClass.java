1a6498d0096100cfd0a528b9810430dd5e6cbc2e
jjuannjjorm <1784360@alu.murciaeduca.es>
2022-09-28 19:24:05 +0200

    proyecto prueba 2

--- src/main/java/DIUT01/NewClass.java
+++ src/main/java/DIUT01/NewClass.java
@@ -23,22 +23,22 @@ public class NewClass {
         Options options = new Options();
     
         options.addOption("w", false, "Hola");          options.addOption("c", false, "Buenas tardes");
-        options.addOption("n", false, "Buenos dias");   options.addOption("h", false, "Hola mundo");
-        options.addOption("y", false, "Adios mundo");
+        options.addOption("n", false, "Fecha");   options.addOption("h", false, "Hola mundo");
+        options.addOption("y", false, "Buenos dias");
         
         
         
         CommandLineParser parser = (CommandLineParser) new org.apache.commons.cli.DefaultParser();
         CommandLine cmd = parser.parse(options, args);
 
-        if (cmd.hasOption("w"))      System.out.println("Hola mundo");
-        else if (cmd.hasOption("c")) System.out.println("Hola mundo cruel");
-        else if (cmd.hasOption("n")) System.out.println("Hola mundo bonito");
-        else if (cmd.hasOption("h")) System.out.println("Pulsa w, n o c para ejecutar una accion de este programa");
-        else if (cmd.hasOption("y")) {
-           DateFormat myFormat = DateFormat.getDateInstance( DateFormat.LONG, Locale.ENGLISH );
+        if (cmd.hasOption("w"))      System.out.println("Hola");
+        else if (cmd.hasOption("c")) System.out.println("Buenas tardes");
+        else if (cmd.hasOption("n")) {
+            DateFormat myFormat = DateFormat.getDateInstance( DateFormat.LONG, Locale.ENGLISH );
            System.out.println(DateFormat.DATE_FIELD);
-        } else System.err.getClass();
+        } else if (cmd.hasOption("h")) System.out.println("Hola mundo");
+        else if (cmd.hasOption("y")) System.out.println("Buenos dias");
+        else System.err.getClass();
         
     
 }
